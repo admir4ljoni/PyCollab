0706b927f27f15f0cf133d2125062463ac3551a9
admir4ljoni <akuncadangannn2020@gmail.com>
2026-05-26 11:38:31 +0700

    feat | added validation

--- Tenaga_Layanan/Perawat/perawat_inap.py
+++ Tenaga_Layanan/Perawat/perawat_inap.py
@@ -1,1 +1,12 @@
 from __future__ import annotations
+
+class PerawatInap(Perawat):
+    def __init__(self, nama: str, nomor_pegawai: str, biaya_per_malam: int):
+        super().__init__(nama, nomor_pegawai)
+
+        if not isinstance(biaya_per_malam, int):
+            raise TypeError("Biaya per malam harus berupa integer")
+        if biaya_per_malam < 0:
+            raise ValueError("Biaya per malam tidak boleh negatif")
+
+        self.biaya_per_malam = biaya_per_malam
