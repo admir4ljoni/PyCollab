47ddfd18a537dde572bb2bacbacfad76940bc1a8
admir4ljoni <akuncadangannn2020@gmail.com>
2026-05-26 10:09:19 +0700

    feat | defined KucingAnggora and KucingKampung

---
+++ Hewan/Kucing/kucing_anggora.py
@@ -0,0 +1,16 @@
+from Kucing.kucing import Kucing
+from Pemilik.pemilik import Pemilik
+
+class KucingAnggora(Kucing):
+    def __init__(
+        self,
+        nama: str,
+        usia: int,
+        berat_badan: float,
+        pemilik: Pemilik,
+        warna: str,
+        perawatan_khusus: list[dict[str, int | str]]
+    ):
+        super().__init__(nama, usia, berat_badan, pemilik)
+        self.warna = warna
+        self.perawatan_khusus = perawatan_khusus
