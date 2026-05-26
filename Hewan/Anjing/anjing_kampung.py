633336f99f09efed9b3c0f354c2109ff6670c709
admir4ljoni <akuncadangannn2020@gmail.com>
2026-05-26 11:09:40 +0700

    feat | completed Hewan

--- Hewan/Anjing/anjing_kampung.py
+++ Hewan/Anjing/anjing_kampung.py
@@ -15,3 +15,14 @@ class AnjingKampung(Anjing):
         super().__init__(nama, usia, berat_badan, pemilik)
         self.warna = warna
         self.perawatan_khusus = perawatan_khusus
+
+    def tampilkan_data(self):
+        print(f"Nama: {self.nama}")
+        print(f"Usia: {self.usia}")
+        print(f"Berat Badan: {self.berat_badan} kg")
+        print(f"Pemilik: {self.pemilik.nama}")
+        print(f"Warna: {self.warna}")
+        for perawatan in self.perawatan_khusus:
+            harga = f"{perawatan['harga']:,.0f}".replace(",", ".")
+            print(f"---> Perawatan Khusus: {perawatan['nama']}\n---> Frekuensi: {perawatan['frekuensi']}x per hari\n---> Harga: Rp{harga}")
+        print()
--- Hewan/hewan.py
+++ Hewan/hewan.py
@@ -12,20 +12,36 @@ class Hewan:
         berat_badan: float, # berat badan di kg
         pemilik: Pemilik
     ):
+        if not isinstance(usia, int):
+            raise TypeError("Usia harus berupa integer")
+        if usia < 0:
+            raise ValueError("Usia tidak boleh negatif")
+        
+        if not isinstance(berat_badan, (float, int)):
+            raise TypeError("Berat badan harus berupa float atau integer")
+        if berat_badan < 0:
+            raise ValueError("Berat badan tidak boleh negatif")
+        
+        if not isinstance(pemilik, Pemilik):
+            raise TypeError("Pemilik harus berupa instance dari kelas Pemilik")
+
         self.nama = nama
         self.usia = usia
         self.berat_badan = berat_badan
         self.pemilik = pemilik
         Hewan._registry.append(self)
 
-    def tampilkan_data(
-        self
-        ):
-        pass
+    def tampilkan_data(self):
+        print(f"Salah memanggil objek {self.__class__.__name__}, silahkan gunakan method tampilkan_data() dari kelas turunan")
 
     @classmethod
     def tampilkan_hewan_berdasarkan_jenis(
         cls,
         jenis_hewan: type
     ):
-        pass
+        print(f"Semua hewan berjenis {jenis_hewan.__name__}:")
+        for hewan in cls._registry:
+            if isinstance(hewan, jenis_hewan):
+                hewan.tampilkan_data()
+                
+        
